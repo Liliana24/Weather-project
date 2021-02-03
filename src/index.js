8f096511c505a3998a38cc041cf628611adefee6
Liliana24 <38566662+Liliana24@users.noreply.github.com>
2021-02-03 23:02:17 +0000

    added API integration

--- src/index.js
+++ src/index.js
@@ -27,4 +27,20 @@ function formatDate(date) {
   let currentTime = new Date();
   dateElement.innerHTML = formatDate(currentTime);
   
+function displayTemperature(response) {
+console.log(response.data.main.temp)
+let temperatureElement = document.querySelector("#degree");
+let cityElement = document.querySelector("#city");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+humidityElement.innerHTML = response.data.main.humidity + "%";
+windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
  let apiKey = "61568445203fe59583ad84f212ca0045";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`
+
+ 
+ axios.get(apiUrl).then(displayTemperature)
