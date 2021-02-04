6ba1e1ceee5eb52eb932d5a0dff47f8ba58e76eb
Liliana24 <38566662+Liliana24@users.noreply.github.com>
2021-02-04 22:14:39 +0000

    changed title

--- src/index.js
+++ src/index.js
@@ -28,19 +28,38 @@ function formatDate(date) {
   dateElement.innerHTML = formatDate(currentTime);
   
 function displayTemperature(response) {
-console.log(response.data.main.temp)
 let temperatureElement = document.querySelector("#degree");
 let cityElement = document.querySelector("#city");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 humidityElement.innerHTML = response.data.main.humidity + "%";
 windElement.innerHTML = Math.round(response.data.wind.speed);
+iconElement.setAttribute(
+  "src", 
+  `http://openweathermap.org/img/wn/${
+    response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
- let apiKey = "61568445203fe59583ad84f212ca0045";
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`
+function search(city) {
+  let apiKey = "61568445203fe59583ad84f212ca0045";  
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayTemperature)
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
  
- axios.get(apiUrl).then(displayTemperature)
+
+
+ let form = document.querySelector("#search_city");
+ form.addEventListener("submit", handleSubmit);
