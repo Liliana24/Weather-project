dd399893dcb9842da3162a6034438b60071d95ad
Liliana24 <38566662+Liliana24@users.noreply.github.com>
2021-02-08 11:30:32 +0000

    amended search input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-function formatDate(date) {
-    let currentHour = date.getHours();
-    if (currentHour < 10) {
-      currentHour = `0${currentHour}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp)
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
     }
-    let currentMinutes = date.getMinutes();
-    if (currentMinutes < 10) {
-      currentMinutes = `0${currentMinutes}`;
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
     }
   
     let dayIndex = date.getDay();
@@ -20,21 +21,49 @@ function formatDate(date) {
     ];
     let day = days[dayIndex];
   
-    return `${day} ${currentHour}:${currentMinutes}`;
+    return `${day} ${hours}:${minutes}`;
   }
   
   let dateElement = document.querySelector("h4");
   let currentTime = new Date();
   dateElement.innerHTML = formatDate(currentTime);
   
+function formatHours(timestamp) {
+  let date = new Date(timestamp)
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let dayIndex = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ]
+    let day = days[dayIndex];
+    
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#degree");
 let cityElement = document.querySelector("#city");
 let humidityElement = document.querySelector("#humidity");
+let descriptionElement = document.querySelector("#description");
 let windElement = document.querySelector("#wind");
 let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity + "%";
 windElement.innerHTML = Math.round(response.data.wind.speed);
 iconElement.setAttribute(
@@ -45,10 +74,44 @@ iconElement.setAttribute(
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    
+    <div class="row">
+      <div class="col-2">
+       <p id="hours">
+       ${formatHours(forecast.dt * 1000)}
+       </p>
+       <img 
+       src ="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+       />
+        <div class="temperatures">
+       <strong>
+          ${Math.round(forecast.main.temp_max)}°
+           </strong> 
+           ${Math.round(forecast.main.temp_min)}°
+        </div>
+      </div>
+    </div>
+
+
+`;
+}
+}
+
 function search(city) {
   let apiKey = "61568445203fe59583ad84f212ca0045";  
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-  axios.get(apiUrl).then(displayTemperature)
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -59,7 +122,5 @@ function handleSubmit(event) {
 
 search("New York");
  
-
-
  let form = document.querySelector("#search_city");
  form.addEventListener("submit", handleSubmit);
